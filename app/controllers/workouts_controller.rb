e5291120fe3fa28ccf29533bacdb3ec590026c5f
Bong <mjbond_bong2@yahoo.com>
2017-03-11 15:25:44 +0800

    create model/controller for workout. created index/show/new/created/edit/destroy all working fine

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,50 @@
+class WorkoutsController < ApplicationController
+	before_action :find, only: [:show, :edit, :destroy, :update]
+
+	def index
+		@workouts = Workout.all.order(created_at: :desc)
+	end
+
+	def show
+	end
+
+	def new
+		@workout = Workout.new
+	end
+
+	def create
+		@workout = Workout.new(workout_params)
+		if @workout.save
+		   redirect_to @workout
+		else 
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @workout.update(workout_params)
+			redirect_to @workout
+		else 
+			render 'edit'
+		end
+
+	end
+
+	def destroy
+		@workout.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def workout_params
+		params.require(:workout).permit(:date, :workout, :mood, :length)
+	end
+
+	def find
+		@workout = Workout.find(params[:id])
+	end
+end
